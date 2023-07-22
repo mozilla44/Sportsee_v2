08c679e21ee0fd18419d8c3baf5d730558fbd1df
Antoine <antoine.frisco@gmail.com>
2023-07-22 11:18:36 +0200

    edited mock manager

--- front-end/src/services/mockManager.js
+++ front-end/src/services/mockManager.js
@@ -13,20 +13,23 @@ async function getUserInfo (id){
 //TODO finish mock api calls 
 
 async function getUserSession (id){
-    let result = await fetch(`http://localhost:3001/user/${id}/average-sessions`)
+    let result = await fetch(`/mock.json`)
     let data = await result.json()
+    data = data.USER_AVERAGE_SESSIONS.find(user => user.id === id)
     return new Session (data.data)
 }
 
 async function getUserActivity (id){
-    let result = await fetch(`http://localhost:3001/user/${id}/activity`)
+    let result = await fetch(`/mock.json`)
     let data = await result.json()
+    data = data.USER_ACTIVITY.find(user => user.id === id)
     return new Activity(data.data)
 }
 
 async function getUserPerf (id){
-    let result = await fetch(`http://localhost:3001/user/${id}/performance`)
+    let result = await fetch(`/mock.json`)
     let data = await result.json()
+    data = data.USER_PERFORMANCE.find(user => user.id === id)
     return  new Performance(data.data)
 }
 
